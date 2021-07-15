9c7f2fc85c5d4cd2e793ce91e7c86cf1cf002c1c
shahrour <ahmadshahrour951@gmail.com>
2021-07-15 10:40:14 -0700

    Rinkeby setup and init dapp setup

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,36 @@
+require('dotenv').config();
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var mnemonic = process.env['NEMONIC'];
+var tokenKey = process.env['ENDPOINT_KEY'];
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: '*', // Match any network id
     },
     develop: {
-      port: 8545
-    }
-  }
+      port: 8545,
+    },
+    rinkeby: {
+      host: "localhost",
+      provider: function () {
+        return new HDWalletProvider(
+          mnemonic,
+          'https://rinkeby.infura.io/v3/' + tokenKey
+        );
+      },
+      network_id: 4,
+      gas: 6700000,
+      gasPrice: 10000000000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.5.0',
+    },
+  },
 };
